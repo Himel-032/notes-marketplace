5a45193cac9075f8e8cf5a06378d0c3d1dd01950
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2026-03-13 00:39:02 +0600

    feature: Enhance NoteRepository and NoteService with additional search and filter methods

--- src/main/java/com/notes/notesmarketplace/repository/NoteRepository.java
+++ src/main/java/com/notes/notesmarketplace/repository/NoteRepository.java
@@ -1,16 +1,26 @@
 package com.notes.notesmarketplace.repository;
 
+import java.util.List;
 
-
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.notes.notesmarketplace.model.Note;
 import com.notes.notesmarketplace.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
     List<Note> findBySeller(User seller);
+
+    List<Note> findByTitleContainingIgnoreCase(String title);
+
+    List<Note> findByDescriptionContainingIgnoreCase(String description);
+
+    List<Note> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
+            String title,
+            String description
+    );
+
+    List<Note> findByCategoryIgnoreCase(String category);
+
 
 }
