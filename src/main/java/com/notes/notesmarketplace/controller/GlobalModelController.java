18c5bcfc1f21a40471d75357149e4c2bcde8e811
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2026-04-04 16:17:09 +0600

    refactor: reorder import statements in GlobalModelController for consistency

--- src/main/java/com/notes/notesmarketplace/controller/GlobalModelController.java
+++ src/main/java/com/notes/notesmarketplace/controller/GlobalModelController.java
@@ -1,15 +1,17 @@
 package com.notes.notesmarketplace.controller;
 
-import com.notes.notesmarketplace.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.notes.notesmarketplace.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @ControllerAdvice
 @RequiredArgsConstructor
-public class GlobalModelController {
+public class GlobalModelController { // runs before every controller method to add common attributes to the model
 
     private final UserRepository userRepository;
 
